94bc4ecd50187bd3af288be1632913c056d9a793
kavitamli13 <kavithanjalitamilselvan@gmail.com>
2026-01-09 14:47:28 +0530

    Update logging_script.sh

--- new_scripts/logging_script.sh
+++ new_scripts/logging_script.sh
@@ -1,5 +1,3 @@
-#Install_script.sh
-
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -11,147 +9,149 @@ STORAGE_CLASS=cinder-standard
 LOKI_STORAGE=50Gi
 TENANT_HEADER="X-Scope-OrgID"
 
-echo "Creating namespaces..."
-kubectl create ns $LOKI_NS --dry-run=client -o yaml | kubectl apply -f -
-kubectl create ns $LOGGING_NS --dry-run=client -o yaml | kubectl apply -f -
-kubectl create ns $GRAFANA_NS --dry-run=client -o yaml | kubectl apply -f -
+usage() {
+  echo "Usage: $0 {install-fluentd|uninstall-fluentd|get-application-info} "
+  exit 1
+}
 
-echo "Adding Helm repos..."
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add fluent https://fluent.github.io/helm-charts
-helm repo update
+function install_fluentd(){
 
-echo "Installing Loki (multi-tenant)..."
-helm upgrade --install loki grafana/loki \
-  -n $LOKI_NS \
-  --set deploymentMode=SingleBinary \
-  --set loki.auth_enabled=true \
-  --set loki.commonConfig.replication_factor=1 \
-  --set loki.storage.type=filesystem \
-  --set loki.storage.filesystem.directory=/var/loki \
-  --set persistence.enabled=true \
-  --set persistence.size=$LOKI_STORAGE \
-  --set persistence.storageClassName=$STORAGE_CLASS \
-  --set limits_config.max_label_names_per_series=15 \
-  --set limits_config.reject_old_samples=true \
-  --set limits_config.reject_old_samples_max_age=168h \
-  --wait
-
-echo "Installing Grafana..."
-helm upgrade --install grafana grafana/grafana \
-  -n $GRAFANA_NS \
-  --set adminPassword=admin \
-  --set service.type=ClusterIP \
-  --set datasources."datasources\.yaml".apiVersion=1 \
-  --set datasources."datasources\.yaml".datasources[0].name=Loki \
-  --set datasources."datasources\.yaml".datasources[0].type=loki \
-  --set datasources."datasources\.yaml".datasources[0].url=http://loki.$LOKI_NS.svc.cluster.local:3100 \
-  --set datasources."datasources\.yaml".datasources[0].access=proxy \
-  --set datasources."datasources\.yaml".datasources[0].jsonData.httpHeaderName1=$TENANT_HEADER \
-  --set datasources."datasources\.yaml".datasources[0].secureJsonData.httpHeaderValue1=default \
-  --wait
-
-echo "Creating Fluent Bit config..."
-cat <<EOF | kubectl apply -n $LOGGING_NS -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: fluent-bit-config
-data:
-  fluent-bit.conf: |
-    [SERVICE]
-        Flush         5
-        Daemon        Off
-        Log_Level     info
-
-    [INPUT]
-        Name              tail
-        Path              /var/log/containers/*.log
-        Parser            docker
-        Tag               kube.*
-        Refresh_Interval  5
-        Rotate_Wait       30
-
-    [FILTER]
-        Name                kubernetes
-        Match               kube.*
-        Merge_Log           On
-        Keep_Log            Off
-
-    [FILTER]
-        Name modify
-        Match kube.*
-        Remove app_kubernetes_io_instance
-        Remove app_kubernetes_io_managed_by
-        Remove app_kubernetes_io_name
-        Remove app_kubernetes_io_part_of
-        Remove strimzi_io_cluster
-        Remove strimzi_io_component_type
-        Remove strimzi_io_controller
-        Remove strimzi_io_controller_name
-        Remove strimzi_io_kind
-        Remove strimzi_io_name
-        Remove strimzi_io_pod_name
-        Remove strimzi_io_pool_name
-
-    [OUTPUT]
-        Name        loki
-        Match       kube.*
-        Host        loki.$LOKI_NS.svc.cluster.local
-        Port        3100
-        Header      $TENANT_HEADER default
-        Labels      job=fluent-bit,namespace=\$kubernetes['namespace_name'],pod=\$kubernetes['pod_name'],container=\$kubernetes['container_name']
-        Auto_Kubernetes_Labels off
+  echo "Creating namespaces..."
+  kubectl create ns $LOKI_NS --dry-run=client -o yaml | kubectl apply -f -
+  kubectl create ns $LOGGING_NS --dry-run=client -o yaml | kubectl apply -f -
+  kubectl create ns $GRAFANA_NS --dry-run=client -o yaml | kubectl apply -f -
+  
+  echo "Adding Helm repos..."
+  helm repo add grafana https://grafana.github.io/helm-charts
+  helm repo add fluent https://fluent.github.io/helm-charts
+  helm repo update
+  
+  echo "Installing Loki (multi-tenant)..."
+  helm upgrade --install loki grafana/loki \
+    -n $LOKI_NS \
+    --set deploymentMode=SingleBinary \
+    --set loki.auth_enabled=true \
+    --set loki.commonConfig.replication_factor=1 \
+    --set loki.storage.type=filesystem \
+    --set loki.storage.filesystem.directory=/var/loki \
+    --set persistence.enabled=true \
+    --set persistence.size=$LOKI_STORAGE \
+    --set persistence.storageClassName=$STORAGE_CLASS \
+    --set limits_config.max_label_names_per_series=15 \
+    --set limits_config.reject_old_samples=true \
+    --set limits_config.reject_old_samples_max_age=168h \
+    --wait
+  
+  echo "Installing Grafana..."
+  helm upgrade --install grafana grafana/grafana \
+    -n $GRAFANA_NS \
+    --set adminPassword=admin \
+    --set service.type=ClusterIP \
+    --set datasources."datasources\.yaml".apiVersion=1 \
+    --set datasources."datasources\.yaml".datasources[0].name=Loki \
+    --set datasources."datasources\.yaml".datasources[0].type=loki \
+    --set datasources."datasources\.yaml".datasources[0].url=http://loki.$LOKI_NS.svc.cluster.local:3100 \
+    --set datasources."datasources\.yaml".datasources[0].access=proxy \
+    --set datasources."datasources\.yaml".datasources[0].jsonData.httpHeaderName1=$TENANT_HEADER \
+    --set datasources."datasources\.yaml".datasources[0].secureJsonData.httpHeaderValue1=default \
+    --wait
+  
+  echo "Creating Fluent Bit config..."
+  cat <<EOF | kubectl apply -n $LOGGING_NS -f -
+  apiVersion: v1
+  kind: ConfigMap
+  metadata:
+    name: fluent-bit-config
+  data:
+    fluent-bit.conf: |
+      [SERVICE]
+          Flush         5
+          Daemon        Off
+          Log_Level     info
+  
+      [INPUT]
+          Name              tail
+          Path              /var/log/containers/*.log
+          Parser            docker
+          Tag               kube.*
+          Refresh_Interval  5
+          Rotate_Wait       30
+  
+      [FILTER]
+          Name                kubernetes
+          Match               kube.*
+          Merge_Log           On
+          Keep_Log            Off
+  
+      [FILTER]
+          Name modify
+          Match kube.*
+          Remove app_kubernetes_io_instance
+          Remove app_kubernetes_io_managed_by
+          Remove app_kubernetes_io_name
+          Remove app_kubernetes_io_part_of
+          Remove strimzi_io_cluster
+          Remove strimzi_io_component_type
+          Remove strimzi_io_controller
+          Remove strimzi_io_controller_name
+          Remove strimzi_io_kind
+          Remove strimzi_io_name
+          Remove strimzi_io_pod_name
+          Remove strimzi_io_pool_name
+  
+      [OUTPUT]
+          Name        loki
+          Match       kube.*
+          Host        loki.$LOKI_NS.svc.cluster.local
+          Port        3100
+          Header      $TENANT_HEADER default
+          Labels      job=fluent-bit,namespace=\$kubernetes['namespace_name'],pod=\$kubernetes['pod_name'],container=\$kubernetes['container_name']
+          Auto_Kubernetes_Labels off
 EOF
+  
+  echo "Installing Fluent Bit..."
+  helm upgrade --install fluent-bit fluent/fluent-bit \
+    -n $LOGGING_NS \
+    --set configMap=fluent-bit-config \
+    --set serviceAccount.create=true \
+    --set tolerations[0].operator=Exists \
+    --set daemonSet=true \
+    --wait
+  
+  echo ""
+  echo "Multi-tenant logging stack installed successfully ??"
+  echo ""
+  echo "Grafana access:"
+  echo "  kubectl port-forward -n $GRAFANA_NS svc/grafana 3000:80"
+  echo "  user: admin  pass: admin"
+  echo ""
 
-echo "Installing Fluent Bit..."
-helm upgrade --install fluent-bit fluent/fluent-bit \
-  -n $LOGGING_NS \
-  --set configMap=fluent-bit-config \
-  --set serviceAccount.create=true \
-  --set tolerations[0].operator=Exists \
-  --set daemonSet=true \
-  --wait
-
-echo ""
-echo "Multi-tenant logging stack installed successfully ??"
-echo ""
-echo "Grafana access:"
-echo "  kubectl port-forward -n $GRAFANA_NS svc/grafana 3000:80"
-echo "  user: admin  pass: admin"
-echo ""
+  get_application_info
+  
+}
 
 
-
-#uninstall script
-
-#!/usr/bin/env bash
-set -euo pipefail
-
-LOKI_NS=loki
-LOGGING_NS=logging
-GRAFANA_NS=grafana
-
-echo "Uninstalling Fluent Bit..."
-helm uninstall fluent-bit -n ${LOGGING_NS} || true
-
-echo "Uninstalling Loki..."
-helm uninstall loki -n ${LOKI_NS} || true
-
-echo "Uninstalling Grafana..."
-helm uninstall grafana -n ${GRAFANA_NS} || true
-
-echo "Deleting namespaces..."
-kubectl delete ns ${LOGGING_NS} --ignore-not-found
-kubectl delete ns ${LOKI_NS} --ignore-not-found
-kubectl delete ns ${GRAFANA_NS} --ignore-not-found
-
-echo "Deleting remaining PVCs..."
-kubectl delete pvc -n ${LOKI_NS} --all --ignore-not-found
-kubectl delete pvc -n ${GRAFANA_NS} --all --ignore-not-found
-
-echo "Logging stack successfully removed."
-
+function uninstall_fluentd() {
+  echo "Uninstalling Fluent Bit..."
+  helm uninstall fluent-bit -n ${LOGGING_NS} || true
+  
+  echo "Uninstalling Loki..."
+  helm uninstall loki -n ${LOKI_NS} || true
+  
+  echo "Uninstalling Grafana..."
+  helm uninstall grafana -n ${GRAFANA_NS} || true
+  
+  echo "Deleting namespaces..."
+  kubectl delete ns ${LOGGING_NS} --ignore-not-found
+  kubectl delete ns ${LOKI_NS} --ignore-not-found
+  kubectl delete ns ${GRAFANA_NS} --ignore-not-found
+  
+  echo "Deleting remaining PVCs..."
+  kubectl delete pvc -n ${LOKI_NS} --all --ignore-not-found
+  kubectl delete pvc -n ${GRAFANA_NS} --all --ignore-not-found
+  
+  echo "Logging stack successfully removed."
+}
 
 function get_application_info() {
   LOKI_NS=loki
@@ -195,3 +195,11 @@ function get_application_info() {
 }
 EOF
 }
+
+ACTION="${1:-}"
+case "$ACTION" in
+    install-fluentd) install_fluentd ;;
+    uninstall-fluentd) uninstall_fluentd ;;
+    get-application-info) get_application_info ;;
+    *) usage ;;
+esac
